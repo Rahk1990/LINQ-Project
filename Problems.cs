783d433fbd11dabd524c76d438f821aa6faaf36c
rahk1990 <rahkem.ellis@gmail.com>
2022-03-29 17:03:06 -0400

    completed problem 9

--- Problems.cs
+++ Problems.cs
@@ -22,8 +22,8 @@ namespace DatabaseFirstLINQ
             //ProblemFive();
             //ProblemSix();
             //ProblemSeven();
-            ProblemEight();
-            //ProblemNine();
+            //ProblemEight();
+            ProblemNine();
             //ProblemTen();
             //ProblemEleven();
             //ProblemTwelve();
@@ -158,6 +158,11 @@ namespace DatabaseFirstLINQ
             // Write a LINQ query that retreives all of the products in the shopping cart of the user who has the email "oda@gmail.com" and returns the sum of all of the products prices.
             // HINT: End of query will be: .Select(sc => sc.Product.Price).Sum();
             // Then print the total of the shopping cart to the console.
+            var userCart = _context.ShoppingCarts.Where(ur => ur.User.Email == "oda@gmail.com").Select(sc => sc.Product.Price).Sum();
+        
+            {
+                Console.WriteLine(userCart);
+            }
 
         }
 
